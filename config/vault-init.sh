c9dcde4da8c35cdd602d020375f5d7b9e63134c3
Jip Spinnewijn <jip.spinnewijn@gmail.com>
2018-12-24 14:09:03 +0100

    Forced the Vault to use K/V version 1, because spring cloud vault is configured to use the version 1 api.

--- config/vault-init.sh
+++ config/vault-init.sh
@@ -4,6 +4,8 @@ VAULT_DEV_TOKEN=934f9eae-31ff-a8ef-e1ca-4bea9e07aa09
 
 vault login ${VAULT_DEV_TOKEN}
 
+vault secrets disable secret
+vault secrets enable -version=1 -path=secret kv
 vault kv put secret/application @${CONFIG_DIR}/application.json
 vault kv put secret/catalog-service @${CONFIG_DIR}/catalog-service.json
 
