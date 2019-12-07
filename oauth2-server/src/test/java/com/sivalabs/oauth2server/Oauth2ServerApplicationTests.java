a3a30959983077a809ed4545407b30ce08ad0ff0
rajadilipkolli <rajadilipkolli@gmail.com>
2019-12-08 03:22:51 +0530

    Upgrades to latest available version.

--- oauth2-server/src/test/java/com/sivalabs/oauth2server/Oauth2ServerApplicationTests.java
+++ oauth2-server/src/test/java/com/sivalabs/oauth2server/Oauth2ServerApplicationTests.java
@@ -1,11 +1,8 @@
 package com.sivalabs.oauth2server;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class Oauth2ServerApplicationTests {
 
