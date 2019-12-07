91e05ff96ab437b46eb0f1972b4502dd04c9d7b0
Raja Kolli <rajadilipkolli@users.noreply.github.com>
2019-12-08 03:30:08 +0530

    sync with original source code (#2)
    * Updated the Vault commands to the latest version
    
    * Forced the Vault to use K/V version 1, because spring cloud vault is configured to use the version 1 api.
    
    * update docker images

--- config/vault-init.sh
+++ config/vault-init.sh
@@ -2,8 +2,10 @@
 
 VAULT_DEV_TOKEN=934f9eae-31ff-a8ef-e1ca-4bea9e07aa09
 
-vault auth ${VAULT_DEV_TOKEN}
+vault login ${VAULT_DEV_TOKEN}
 
-vault write secret/application @${CONFIG_DIR}/application.json
-vault write secret/catalog-service @${CONFIG_DIR}/catalog-service.json
+vault secrets disable secret
+vault secrets enable -version=1 -path=secret kv
+vault kv put secret/application @${CONFIG_DIR}/application.json
+vault kv put secret/catalog-service @${CONFIG_DIR}/catalog-service.json
 
