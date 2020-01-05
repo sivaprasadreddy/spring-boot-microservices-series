71df243edf24ef805e17f4cfb5a25c880fd230c2
rajadilipkolli <rajadilipkolli@gmail.com>
2020-01-05 10:53:17 +0530

    removing cloud contract verifier

---
+++ catalog-service/src/test/java/com/sivalabs/catalogservice/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sivalabs.catalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
--- catalog-service/src/test/resources/contracts/find_by_product_code.groovy
+++ catalog-service/src/test/resources/contracts/find_by_product_code.groovy
@@ -1,14 +1,13 @@
 package contracts
 
 import org.springframework.cloud.contract.spec.Contract
-import org.springframework.cloud.contract.verifier.util.ContractVerifierUtil
 
 Contract.make {
     description('Find Product By Code')
     request {
         urlPath('/api/products') {
             queryParameters {
-                parameter 'code': value(consumer(matching("[a-zA-Z0-9]+")), producer('P001'))
+                parameter("code", value(consumer(matching("[a-zA-Z0-9]+")), producer('P001')))
             }
         }
         method GET()
@@ -19,12 +18,11 @@ Contract.make {
     response {
         status OK()
         body(
-                [
-                        "code"       : "P001",
-                        "name"       : "Product 1",
-                        "description": "Product 1 description",
-                        "price"      : 25
-                ]
+                "id": value(producer(regex("[0-9]+")), consumer("1")),
+                "code": fromRequest().query("code"),
+                "name": value(producer(regex("[A-Za-z0-9]+"))),
+                "description": value(producer(regex("[A-Za-z0-9]+"))),
+                "price": value(producer(regex("[0-9].[0-9]+")))
         )
         headers {
             contentType(applicationJson())
