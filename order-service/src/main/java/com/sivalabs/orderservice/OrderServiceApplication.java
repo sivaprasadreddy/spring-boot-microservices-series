26d3b6f9e16fb98164387f03cc3a2d33e1fc3cf7
rajadileepkolli <rajadileepkolli@gmail.com>
2021-12-29 23:32:07 +0530

    upgrade order service to latest version

--- order-service/src/main/java/com/sivalabs/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/sivalabs/orderservice/OrderServiceApplication.java
@@ -1,14 +1,29 @@
 package com.sivalabs.orderservice;
 
+import java.time.Duration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
 
-@EnableCircuitBreaker
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+
 @SpringBootApplication
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+	@Bean
+	public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+		return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+				.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build())
+				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+				.build());
 	}
 }
